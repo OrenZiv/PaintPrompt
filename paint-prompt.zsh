45dfc56c40a9dc1004e2e4599032f8d7571d473e
Oren Ziv <oren.ziv@fuga.com>
2022-09-05 10:11:33 +0200

    Update paint-prompt.zsh

--- paint-prompt.zsh
+++ paint-prompt.zsh
@@ -4,12 +4,12 @@ init_prompt () {
 }
 
 paint_prompt() {
-    PROD=$'MY_K8S_PROD_CONTEXT'
+    PROD=$'MY_K8S_PROD_CONTEXT' # Change to your production context.
     newline=$'\n'
 
-    PROMPT='%{$fg[green]%}%n@%m %{$fg[cyan]%}%~'
-    PROMPT+='$([[ $(k8s_context) =~ ${PROD} ]] && echo %{$fg_bold[red]%} || echo %{$fg[yellow]%})$(k8s_context) '
-    PROMPT+='%{$fg_bold[blue]%}$(git_prompt_info)%{$reset_color%}'
+    PROMPT='%{$fg[green]%}%n@%m %{$fg[cyan]%}%~' # Username and current folder.
+    PROMPT+='$([[ $(k8s_context) =~ ${PROD} ]] && echo %{$fg_bold[red]%} || echo %{$fg[yellow]%})$(k8s_context) ' # Kubernetes context. red if prod, else yellow.
+    PROMPT+='%{$fg_bold[blue]%}$(git_prompt_info)%{$reset_color%}' # Current Git branch.
     PROMPT+='${newline} -> '
 
     ZSH_THEME_GIT_PROMPT_PREFIX="("
@@ -18,7 +18,7 @@ paint_prompt() {
     ZSH_THEME_GIT_PROMPT_CLEAN=" ✔"
 }
 
-# echo current context
+# Echo current Kubernetes context.
 k8s_context() {
     CONTEXT=$(cat ~/.kube/config | grep "current-context:" | sed "s/current-context: //")
 
@@ -40,5 +40,5 @@ set_shortcuts() {
    alias ...........='cd ../../../../../../../../../..'
    alias ............='cd ../../../../../../../../../../..'
 
-   alias rr='cd $(git rev-parse --show-toplevel)' # cd to repo root
+   alias rr='cd $(git rev-parse --show-toplevel)' # cd to repo's root.
 }
